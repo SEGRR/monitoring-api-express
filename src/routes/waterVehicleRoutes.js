0a85b5485ab54f734a6544c11eca25d7388e48bc
shaikh sufiyan <61743021+SEGRR@users.noreply.github.com>
2025-03-17 00:09:52 +0530

    error

--- src/routes/waterVehicleRoutes.js
+++ src/routes/waterVehicleRoutes.js
@@ -5,6 +5,7 @@ import {
   getWaterVehicleById,
   updateWaterVehicle,
   deleteWaterVehicle,
+  getWaterVehicleByPhoneNumber
 } from "../controllers/waterVehicleController.js";
 import upload from "../middleware/uploadMiddleware.js"; // Middleware for handling image uploads
 
